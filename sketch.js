7fd9662dd0857a677771d0b924c4aaf4145a71a1
ambrosia-fish <josefameur@gmail.com>
2024-06-11 17:38:45 -0400

    Delete sketch.js

--- sketch.js
+++
@@ -1,8 +0,0 @@
-collections.forEach(function(collectionName) {
-    const sourceCollection = db.getSiblingDB('users').getCollection(collectionName);
-    const targetCollection = db.getCollection(collectionName);
-    
-    sourceCollection.find().forEach(function(doc) {
-        targetCollection.insert(doc);
-    });
-});
