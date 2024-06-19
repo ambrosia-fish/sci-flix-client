efe93fbcff9f58465fe618ca677292e61732991e
ambrosia-fish <josefameur@gmail.com>
2024-06-19 16:00:56 -0400

    revert to 4c88aea

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,79 +1,45 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card.jsx";
 import { MovieView } from "../movie-view/movie-view.jsx";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            "Poster":"https://upload.wikimedia.org/wikipedia/en/thumb/1/11/2001_A_Space_Odyssey_%281968%29.png/220px-2001_A_Space_Odyssey_%281968%29.png",
-            "Title": "2001: A Space Odyssey",
-            "Description": "A visually stunning sci-fi epic exploring human evolution, AI, and cosmic mysteries in deep space.",
-            "Genre": {
-              "Name": "Epic",
-              "Description": "Epic films are a style of filmmaking with large-scale, sweeping scope, and spectacle."
-            },
-            "Director": {
-              "Name": "Stanley Kubrick",
-              "Bio": "Stanley Kubrick was an American film director, producer, screenwriter, and photographer.",
-              "Birth": "1928",
-              "Death": "1999"
-            }
-          },
-          {
-            "Poster":"https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Blade_Runner_%281982_poster%29.png/220px-Blade_Runner_%281982_poster%29.png",
-            "Title": "Blade Runner",
-            "Description": "In a dystopian future, a blade runner hunts down rogue androids while grappling with his own humanity.",
-            "Genre": {
-              "Name": "Neo-noir",
-              "Description": "Neo-noir is a modern or contemporary motion picture rendition of film noir, emphasizing cynical attitudes and sexual motivations."
-            },
-            "Director": {
-              "Name": "Ridley Scott",
-              "Bio": "Sir Ridley Scott is an English film director and producer, known for his stylish visuals and innovative storytelling.",
-              "Birth": "1937",
-              "Death": ""
-            }
-          },
-          {
-            "Poster": "https://upload.wikimedia.org/wikipedia/en/c/c3/Alien_movie_poster.jpg",
-            "Title": "Alien",
-            "Description": "The crew of the Nostromo battles a terrifying alien life form in this suspenseful and claustrophobic horror classic.",
-            "Genre": {
-              "Name": "Horror",
-              "Description": "Horror films are intended to, or have the capacity to frighten, scare, or disgust."
-            },
-            "Director": {
-              "Name": "Ridley Scott",
-              "Bio": "Sir Ridley Scott is an English film director and producer, known for his stylish visuals and innovative storytelling.",
-              "Birth": "1937",
-              "Death": ""
-            }
-            }
-        ]);
-
+    const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
 
-    if(selectedMovie) {
+    useEffect(() => {
+        fetch("https://sci-flix-075b51101639.herokuapp.com/movies/")
+            .then((response) => response.json())
+            .then((movies) => {
+                const moviesFromApi = movies.map((movie) => ({
+                    _id: movie._id,
+                    title: movie.Title,
+                    genre: movie.Genre.Name,
+                    director: movie.Director.Name
+                }));
+                setMovies(moviesFromApi);
+            })
+            .catch(error => {
+                console.error("Error fetching movies:", error);
+            });
+    }, []);
+    
+
+    if (selectedMovie) {
         return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)}/>
         );
     }
 
-    if(movies.length === 0) {
-        return <div>The list is empty</div>
-    };
+    if (movies.length === 0) {
+        return <div>The list is empty</div>;
+    }
+
 
     return (
         <div>
-            {movies.map((movie) => (
-                <MovieCard
-                key={movie.Title}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                    setSelectedMovie(newSelectedMovie);
-                }}
-                />
-            ))}
+          {movies.map((movie) => (
+            <div key={movie._id} onClick={(newSelectedMovie) => {setSelectedMovie(movie)} }>{movie.title}</div>
+          ))}
         </div>
-    );
-};
+      );
+    };
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({movie, onMovieClick}) => {
     return (
         <div
@@ -8,4 +10,18 @@ export const MovieCard = ({movie, onMovieClick}) => {
             {movie.Title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        _id: PropTypes.string,
+        Title: PropTypes.string,
+        Director: PropTypes.shape({
+            Name: PropTypes.string
+        }),
+        Genre: PropTypes.shape({
+           Name: PropTypes.string
+        }),
+    }).isRequired,
+    onBackClick: PropTypes.func.isRequired
 };
