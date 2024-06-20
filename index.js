b6ab4c343bd0bf899829f07462387f2b71662fc5
ambrosia-fish <josefameur@gmail.com>
2024-06-20 13:27:04 -0400

    removed auth on get /users/

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ app.get('/movies/director/:directorName', passport.authenticate('jwt', { session
 
 
 // get request returns all users 
-app.get('/users', passport.authenticate('jwt', { session: false }), async (req,res) => {
+app.get('/users', async (req,res) => {
     await Users.find()
     .then((users) => {
         res.status(201).json(users);
