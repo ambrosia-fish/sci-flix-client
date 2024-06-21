20182d5e22a5c588a2a4d381305da497c041ea2f
ambrosia-fish <josefameur@gmail.com>
2024-06-21 17:32:18 -0400

    Reformatted MovieView using Bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,16 +1,20 @@
+import "./movie-view.scss";
+import Button from "react-bootstrap/Button"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
-            <div>
-                <span>Title: {movie.title}</span>
-            </div>
-            <div>
-                <span>Subgenre: {movie.genre}</span>
-            </div>
-            <div>
-                <span>Director: {movie.director}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
+            <Card className="h-100 ">
+                <img className="moviePoster" src="https://placehold.co/400x600" />
+                <Card.Body style={{}}>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>Directed by: {movie.director}</Card.Text>
+                </Card.Body>
+            </Card> 
+            <Button onClick={onBackClick} className="back-button">Back</Button>
         </div>
     );
 };
