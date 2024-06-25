665db75544697c555de3bcb736dc932a11d55440
ambrosia-fish <josefameur@gmail.com>
2024-06-25 17:38:10 -0400

    Changed endpoint for updating favorite

---
+++ movie-view.jsx
@@ -0,0 +1,27 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card"
+import "./movie-view.scss";
+import { Button } from "react-bootstrap";
+
+export const MovieView = ({ movies }) => {
+    const { movieId } = useParams();
+
+    const movie = movies.find((m) => m.id === movieId);
+
+    return (
+        <div>
+            <Card className="h-100 ">
+                <img className="moviePoster" src="https://placehold.co/400x600" />
+                <Card.Body style={{}}>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>Directed by: {movie.director}<br /> Subgenre: {movie.genre}</Card.Text>
+                    <Button onClick={toggleFavorite}>Add to favorites</Button>
+                </Card.Body>
+            </Card>
+            <Link to={`/`}>
+                <button className="back-button">Back</button>
+            </Link>
+        </div>
+    );
+}
