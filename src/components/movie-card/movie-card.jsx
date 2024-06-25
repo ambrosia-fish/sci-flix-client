d098178d5500a027a07187dae0366e4977d3f21c
ambrosia-fish <josefameur@gmail.com>
2024-06-25 12:18:40 -0400

    Refactor MovieCard for clientside routing

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types";
-import {Button, Card} from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export const MovieCard = ({movie, onMovieClick}) => {
     return (
@@ -8,9 +10,11 @@ export const MovieCard = ({movie, onMovieClick}) => {
             <Card.Body style={{}}>
                 <Card.Title>{movie.title}</Card.Title>
                 <Card.Text>Directed by: {movie.director}</Card.Text>
-                <Button onClick={() => onMovieClick(movie)} variant="link">
-                    Open
-                </Button>
+                <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+                    <Button variant="link">
+                        Open
+                    </Button>
+                </Link>
             </Card.Body>
         </Card>
     );
