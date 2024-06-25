19ba299eef7f43195e33683bdbcba84cb5a96e17
ambrosia-fish <josefameur@gmail.com>
2024-06-25 18:17:13 -0400

    Added Profile to navigation bar

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -26,7 +26,10 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                 <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                <Nav.Link as={Link} to="/profile/">
+                  Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut} to="/">Logout</Nav.Link>
               </>
             )}
           </Nav>
