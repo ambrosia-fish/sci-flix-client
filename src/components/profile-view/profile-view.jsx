410cda81e1f4edc61c3757628e0198e8cc192942
ambrosia-fish <josefameur@gmail.com>
2024-06-26 13:19:58 -0400

    Refactored profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,15 +1,33 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useState, useEffect } from "react";
 import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Col from "react-bootstrap/Col";
+import { MovieCard } from "../movie-card/movie-card.jsx";
 
+export const ProfileView = ({ movies, user }) => {
+  const [refreshedUser, setRefreshedUser] = useState(null);
 
+  useEffect(() => {
+    fetch(`https://sci-flix-075b51101639.herokuapp.com/users/${user.username}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setRefreshedUser(data); 
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [user.username]); 
 
-
-export const ProfileView = ({ movies, onUpdateUser, onDeregister }) => {
-    const user = JSON.parse(localStorage.getItem('user'));
-    favoriteMovieList = user.favoriteMovies
-
+ 
+  let favoriteMovies = [];
+  if (refreshedUser) {
+    favoriteMovies = movies.filter((m) => refreshedUser.favoriteMovies.includes(m.id));
+  }
 
   return (
     <div>
@@ -17,14 +35,20 @@ export const ProfileView = ({ movies, onUpdateUser, onDeregister }) => {
         <Card.Body>
           <Card.Title>User Profile</Card.Title>
           <Card.Text>
-            Username: {user.username}  <br />
-            Email: {user.email} <br />          
-            Favorite Movies: 
-              <ul>
-              {favoriteMovieList.map((movie, index) => (
-                <li key={index}>{movie}</li>
-              ))}
-            </ul>
+            Username: {user.username} <br />
+            Email: {user.email} <br />
+            Favorite Movies:
+            {favoriteMovies.length > 0 ? (
+              <>
+                {favoriteMovies.map((movie) => (
+                  <Col className="mb-4" key={movie.title} md={3}>
+                    <MovieCard key={movie.id} movie={movie} />
+                  </Col>
+                ))}
+              </>
+            ) : (
+              <p>No favorite movies found.</p>
+            )}
           </Card.Text>
           <Link to="/profile/edit">Edit Profile</Link>
           <br />
@@ -34,6 +58,3 @@ export const ProfileView = ({ movies, onUpdateUser, onDeregister }) => {
     </div>
   );
 };
-
-// create buttons that corresspond to the requests / endpoints for updating all info.
-// a form will have to be created and we can use that data and send it via the request
